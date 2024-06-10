d2c8df8842f961fac3dc424198db0f2fa2e32a15
amir <amir1378khalili@gmail.com>
2024-06-10 16:33:10 +0330

    ADD proccessQueries

--- src/api/resources/car/car.middleware.js
+++ src/api/resources/car/car.middleware.js
@@ -1,3 +1,25 @@
-class CarMiddleware {}
+import User from '../user/user.model.js';
+
+class CarMiddleware {
+  static async proccessQueries(req, res, next) {
+    try {
+      let itemQueries = {};
+
+      if (req.query.age) {
+        const users = await User.find({ age: { $gt: req.query.age } }).select('_id');
+        const userIds = users.map((user) => user._id.toString());
+
+        itemQueries.owner = { $in: userIds };
+      }
+      if (req.query.color) itemQueries.color = { $eq: req.query.color };
+
+      req.itemQueries = itemQueries;
+      return next();
+    } catch (error) {
+      console.error(error);
+      res.status(500).json('an error occurred please try again later');
+    }
+  }
+}
 
 export default CarMiddleware;
