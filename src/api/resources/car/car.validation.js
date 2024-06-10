24242ca00e54dc55984c89bd8d75bbd5677bb53d
amir <amir1378khalili@gmail.com>
2024-06-10 16:33:31 +0330

    FIX error

--- src/api/resources/car/car.validation.js
+++ src/api/resources/car/car.validation.js
@@ -1,11 +1,11 @@
 import Joi from 'joi';
 
 const newCarSchema = Joi.object().keys({
-  type: Joi.string().required(),
+  owner: Joi.string().required(),
+  type: Joi.string().valid('small', 'big').required(),
   color: Joi.string().required(),
   length: Joi.number().required(),
   loadVolume: Joi.number().optional(),
-  owner: Joi.string().required(),
 });
 
 export default newCarSchema;
