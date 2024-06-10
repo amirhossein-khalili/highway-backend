66caa910ea077a6a74c333563b307a1d50707efc
amir <amir1378khalili@gmail.com>
2024-06-10 16:32:22 +0330

    REMOVE song

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,4 @@
 import express from 'express';
-import SongRouter from './resources/song/song.router.js';
 import AuthRouter from './resources/auth/auth.router.js';
 import UserRouter from './resources/user/user.router.js';
 import CarRouter from './resources/car/car.router.js';
@@ -7,7 +6,6 @@ import CartrackerRouter from './resources/cartracker/cartracker.router.js';
 
 export const restRouter = express.Router();
 restRouter.use('/auth', AuthRouter);
-restRouter.use('/songs', SongRouter);
 restRouter.use('/users', UserRouter);
 restRouter.use('/cars', CarRouter);
 restRouter.use('/cartracker', CartrackerRouter);
