417c6f44267d4e1bdef889152359c32952fc21b5
amir <amir1378khalili@gmail.com>
2024-06-11 19:22:39 +0330

    ADD roads

--- src/api/index.js
+++ src/api/index.js
@@ -3,9 +3,11 @@ import AuthRouter from './resources/auth/auth.router.js';
 import UserRouter from './resources/user/user.router.js';
 import CarRouter from './resources/car/car.router.js';
 import CartrackerRouter from './resources/cartracker/cartracker.router.js';
+import RoadRouter from './resources/road/road.router.js';
 
 export const restRouter = express.Router();
 restRouter.use('/auth', AuthRouter);
 restRouter.use('/users', UserRouter);
 restRouter.use('/cars', CarRouter);
 restRouter.use('/cartracker', CartrackerRouter);
+restRouter.use('/roads', RoadRouter);
