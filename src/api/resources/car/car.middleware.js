26781c05006774c1d160a6b6b0190433b888755e
amir <amir1378khalili@gmail.com>
2024-06-11 22:17:42 +0330

    ADD heavyCars

--- src/api/resources/car/car.middleware.js
+++ src/api/resources/car/car.middleware.js
@@ -1,4 +1,5 @@
 import User from '../user/user.model.js';
+import Car from '../car/car.model.js';
 
 class CarMiddleware {
   static async proccessQueries(req, res, next) {
@@ -12,8 +13,20 @@ class CarMiddleware {
         itemQueries.owner = { $in: userIds };
       }
       if (req.query.color) itemQueries.color = { $eq: req.query.color };
+      if (req.query.type) itemQueries.type = { $eq: req.query.type };
 
       req.itemQueries = itemQueries;
+      return next();
+    } catch (error) {
+      console.error(error);
+      res.status(500).json('an error occurred please try again later');
+    }
+  }
+
+  static async heavyCars(req, res, next) {
+    try {
+      const heavyCars = await Car.find({ type: 'big' }).distinct('_id');
+      req.heavyCars = heavyCars;
       return next();
     } catch (error) {
       console.error(error);
