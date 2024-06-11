354be3f04057cd5056f1ca2ff9b883fcb1877d7e
amir <amir1378khalili@gmail.com>
2024-06-11 22:18:38 +0330

    ADD RoadMiddleware

--- src/api/resources/road/road.middleware.js
+++ src/api/resources/road/road.middleware.js
@@ -1,3 +1,18 @@
-class RoadMiddleware {}
+import Road from './road.model.js';
+
+class RoadMiddleware {
+  static async narrowRoads(req, res, next) {
+    try {
+      const roads = await Road.find({ width: { $lt: 20 } })
+        .select('_id name width location')
+        .limit(10);
+      req.narrowRoads = roads;
+      return next();
+    } catch (error) {
+      console.error(error);
+      res.status(500).json('an error occurred please try again later');
+    }
+  }
+}
 
 export default RoadMiddleware;
