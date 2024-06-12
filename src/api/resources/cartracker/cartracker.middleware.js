472fede8e8947348302bdc387fb8f91dca3e54d8
amir <amir1378khalili@gmail.com>
2024-06-12 16:47:56 +0330

    ADD processQueries

--- src/api/resources/cartracker/cartracker.middleware.js
+++ src/api/resources/cartracker/cartracker.middleware.js
@@ -1,3 +1,34 @@
-class CartrackerMiddleware {}
+class CartrackerMiddleware {
+  static async processQueries(req, res, next) {
+    try {
+      let itemQueries = {};
+
+      // Filter by startTime
+      if (req.query.startTime) {
+        const startTime = new Date(req.query.startTime);
+        itemQueries.date = { ...itemQueries.date, $gte: startTime };
+      }
+
+      // Filter by endTime
+      if (req.query.endTime) {
+        const endTime = new Date(req.query.endTime);
+        itemQueries.date = { $lte: endTime };
+      }
+
+      if (req.query.startTime && req.query.endTime) {
+        const startTime = new Date(req.query.startTime);
+        const endTime = new Date(req.query.endTime);
+
+        itemQueries.date = { $gte: startTime, $lte: endTime };
+      }
+
+      req.itemQueries = itemQueries;
+      return next();
+    } catch (error) {
+      console.error(error);
+      res.status(500).json('An error occurred, please try again later');
+    }
+  }
+}
 
 export default CartrackerMiddleware;
