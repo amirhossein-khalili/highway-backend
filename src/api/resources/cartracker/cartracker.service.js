fd33e83ac37379b2c63c7b5d331f92e17c47965b
amir <amir1378khalili@gmail.com>
2024-06-12 22:34:34 +0330

    ADD calculateDistanceAndCompare

--- src/api/resources/cartracker/cartracker.service.js
+++ src/api/resources/cartracker/cartracker.service.js
@@ -9,27 +9,6 @@ import {
 } from '@turf/turf';
 
 class CartrackerService {
-  static calculateDistance = (point1, point2) => {
-    try {
-      const [lon1, lat1] = point1;
-      const [lon2, lat2] = point2;
-      const R = 6371e3; // meters
-      const dLat = ((lat2 - lat1) * Math.PI) / 180;
-      const dLon = ((lon2 - lon1) * Math.PI) / 180;
-      const a =
-        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos((lat1 * Math.PI) / 180) *
-          Math.cos((lat2 * Math.PI) / 180) *
-          Math.sin(dLon / 2) *
-          Math.sin(dLon / 2);
-      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-      return R * c;
-    } catch (error) {
-      console.error(error);
-      res.status(500).json('an error occurred please try again later');
-    }
-  };
-
   static isPointOnLineString = (inputPoint, inputLine) => {
     try {
       const threshold = config.threshold;
@@ -42,6 +21,33 @@ class CartrackerService {
       const isNearLine = distance <= threshold;
 
       return { distance: distance, isNearLine: isNearLine, isOnLine: isOnLine };
+    } catch (error) {
+      console.error(error);
+      res.status(500).json('an error occurred please try again later');
+    }
+  };
+
+  static calculateDistanceAndCompare = (point1, point2, threshold) => {
+    try {
+      // Convert input points to turf.js points
+      const pt1 = _point(point1);
+      const pt2 = _point(point2);
+
+      // Calculate the distance between points in kilometers
+      const distance = _distance(pt1, pt2, { units: 'kilometers' });
+
+      // Check if the distance is less than, equal to, or greater than the threshold
+      let comparisonResult = '';
+      if (distance < threshold) {
+        comparisonResult = 'less';
+      } else if (distance > threshold) {
+        comparisonResult = 'greater';
+      } else {
+        comparisonResult = 'equal';
+      }
+
+      // Return the distance and comparison result
+      return { distance: distance, comparisonResult: comparisonResult };
     } catch (error) {
       console.error(error);
       res.status(500).json('an error occurred please try again later');
