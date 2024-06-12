75063ed6eb2278056ffb3f495afed0595a05345c
amir <amir1378khalili@gmail.com>
2024-06-12 16:48:52 +0330

    ADD isPointOnLineString

--- src/api/resources/cartracker/cartracker.service.js
+++ src/api/resources/cartracker/cartracker.service.js
@@ -1,3 +1,13 @@
+import config from '../../../config/config.js';
+
+import {
+  lineString,
+  point as _point,
+  booleanPointOnLine,
+  nearestPointOnLine,
+  distance as _distance,
+} from '@turf/turf';
+
 class CartrackerService {
   static calculateDistance = (point1, point2) => {
     try {
@@ -14,6 +24,24 @@ class CartrackerService {
           Math.sin(dLon / 2);
       const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
       return R * c;
+    } catch (error) {
+      console.error(error);
+      res.status(500).json('an error occurred please try again later');
+    }
+  };
+
+  static isPointOnLineString = (inputPoint, inputLine) => {
+    try {
+      const threshold = config.threshold;
+      const line = lineString(inputLine);
+      const point = _point(inputPoint);
+
+      const isOnLine = booleanPointOnLine(point, line);
+      const nearestPoint = nearestPointOnLine(line, point);
+      const distance = _distance(point, nearestPoint, { units: 'kilometers' });
+      const isNearLine = distance <= threshold;
+
+      return { distance: distance, isNearLine: isNearLine, isOnLine: isOnLine };
     } catch (error) {
       console.error(error);
       res.status(500).json('an error occurred please try again later');
