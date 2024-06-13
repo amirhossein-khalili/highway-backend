7bb63f8046048fc7f6e491fc7e4596e390355aba
amir <amir1378khalili@gmail.com>
2024-06-13 10:07:43 +0330

    ENHANCE TollController

--- src/api/resources/toll/toll.controller.js
+++ src/api/resources/toll/toll.controller.js
@@ -1,68 +1,49 @@
-import Toll from './toll.model.js';
+import Station from '../station/station.model.js';
+import Car from '../car/car.model.js';
+import CartrackerService from '../cartracker/cartracker.service.js';
+import TollHelper from './toll.helper.js';
+import TollService from './toll.service.js';
 
 class TollController {
-  static selectionToll = 'name tollPerCross location';
-  static selectionTolls = 'name tollPerCross location';
+  static selectionCartracker = 'type color length';
+  static selectionStation = 'name tollPerCross location';
 
-  static async create(req, res, next) {
+  static async user(req, res, next) {
     try {
-      const newToll = new Toll(req.body);
-      await newToll.save();
-      res.status(201).json(newToll);
+      const userId = req.query.userId || String(req.user._id);
+      const cars = await Car.find({ owner: userId })
+        .select(TollController.selectionCartracker)
+        .lean();
+
+      const queryItems = req.itemQueries || {};
+      let tolls = [];
+      let totalTollUser = 0;
+      for (const car of cars) {
+        const carId = car._id;
+        const query = queryItems;
+
+        query.car = { $eq: carId };
+        const resp = await TollService.car(query);
+        totalTollUser = totalTollUser + resp.totalToll;
+
+        tolls.push({ car: car, toll: resp });
+      }
+      // console.log(req.)
+      res.status(200).json({ tolls, totalTollUser });
     } catch (error) {
       console.error(error);
       res.status(500).json('an error occurred please try again later');
     }
   }
 
-  static async findAll(req, res, next) {
+  static async car(req, res, next) {
     try {
-      const { page, perPage } = req.query;
-      const options = {
-        page: parseInt(page, 10) || 1,
-        limit: parseInt(perPage, 10) || 10,
-        select: TollController.selectionTolls,
-      };
+      const carId = req.params.carId;
+      const query = req.itemQueries || {};
+      query.car = { $eq: carId };
+      const resp = await TollService.car(query);
 
-      const tolls = await Toll.paginate({}, options);
-
-      return res.json(tolls);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json('an error occurred please try again later');
-    }
-  }
-
-  static async findOne(req, res, next) {
-    try {
-      const toll = await Toll.findById(req.params.id).select(TollController.selectionTolls);
-      if (!toll) return res.status(404).json({ message: 'toll not found' });
-      return res.json(toll);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json('an error occurred please try again later');
-    }
-  }
-
-  static async edit(req, res, next) {
-    try {
-      const tollUpdated = await Toll.findByIdAndUpdate(req.params.id, req.body, {
-        new: false,
-      });
-      if (!tollUpdated) return res.status(404).json({ message: 'toll not found ' });
-
-      res.json(tollUpdated);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json('an error occurred please try again later');
-    }
-  }
-
-  static async destroy(req, res, next) {
-    try {
-      const tollRemoved = await Toll.findByIdAndDelete(req.params.id);
-      if (!tollRemoved) return res.status(404).json({ message: 'toll not found ' });
-      res.json(tollRemoved);
+      res.status(200).json(resp);
     } catch (error) {
       console.error(error);
       res.status(500).json('an error occurred please try again later');
