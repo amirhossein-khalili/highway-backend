2f87ed4629fbe24334e620d72d77cfa307e647ca
amir <amir1378khalili@gmail.com>
2024-06-13 10:08:22 +0330

    ADD getCloseTimePairs

--- src/api/resources/toll/toll.helper.js
+++ src/api/resources/toll/toll.helper.js
@@ -1,3 +1,26 @@
-class TollHelper {}
+class TollHelper {
+  static getCloseTimePairs = (carTrackers, timeDiffInMinutes) => {
+    try {
+      const result = [];
+      const timeDiffInMilliseconds = timeDiffInMinutes * 60 * 1000;
+
+      for (let i = 0; i < carTrackers.length; i++) {
+        for (let j = i + 1; j < carTrackers.length; j++) {
+          const date1 = new Date(carTrackers[i].date);
+          const date2 = new Date(carTrackers[j].date);
+          const timeDiff = Math.abs(date1 - date2);
+          if (timeDiff <= timeDiffInMilliseconds) {
+            result.push([carTrackers[i], carTrackers[j]]);
+          }
+        }
+      }
+
+      return result;
+    } catch (error) {
+      console.error(error);
+      res.status(500).json('an error occurred please try again later');
+    }
+  };
+}
 
 export default TollHelper;
